b4d214dde044a4204a75843425347179c5d5d171
Mohamed salem <mohamed27035@gmail.com>
2026-06-14 18:13:08 +0100

    middleware

--- config/settings.py
+++ config/settings.py
@@ -77,6 +77,7 @@ INSTALLED_APPS = [
 
 
 MIDDLEWARE = [
+    'apps.core.security_middleware.SecurityMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'corsheaders.middleware.CorsMiddleware',
